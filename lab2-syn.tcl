55d734bc56a2c5a322f420b8747879c642b0f170
Nachiket Kapre <nachiket@uwaterloo.ca>
2019-02-12 11:15:49 -0500

    Update lab2-syn.tcl based on Hugo's comments about SystemVerilog needs for $clog2. Worked fine in our testing as we used $clog2 **only** on the top-level ports and not internally.

--- lab2-syn.tcl
+++ lab2-syn.tcl
@@ -1,6 +1,6 @@
-create_project lab2 ./lab2_vivado -part xc7z020clg400-1
+create_project -force lab2 ./lab2_vivado -part xc7z020clg400-1
 set_property board_part www.digilentinc.com:pynq-z1:part0:1.0 [current_project]
-read_verilog counter.v
+read_verilog -sv counter.v
 synth_design -generic WIDTH=32 -generic HEIGHT=32 -top counter -mode out_of_context;
 opt_design
 report_utilization -file utilization.txt
